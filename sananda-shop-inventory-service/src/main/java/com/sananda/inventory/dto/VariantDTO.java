07d5ef848ebeab025a844f6ac1decbef28bac48f
niladri sen <sen.niladri6@gmail.com>
2021-05-17 00:30:29 +0530

    variant validation

--- sananda-shop-inventory-service/src/main/java/com/sananda/inventory/dto/VariantDTO.java
+++ sananda-shop-inventory-service/src/main/java/com/sananda/inventory/dto/VariantDTO.java
@@ -3,15 +3,22 @@ package com.sananda.inventory.dto;
 import java.math.BigDecimal;
 
 import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
 @Data
 public class VariantDTO {
 	private Long id;
-	private String code;
+	private String sku;
+	
+	@NotNull(message = "mrp is required")
 	private BigDecimal mrp;
+	
 	private BigDecimal discount;
+	
 	private String size;
+	@NotBlank(message = "colour may not be null")
 	private String colour;
 }
